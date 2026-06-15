a92f79dcf3e5130b8393490d3fd4d87904a301a1
unaisah_ <unaiiunais207@gmail.com>
2026-06-15 16:08:46 +0700

    fix: resize favicon 930x906->32x32 (276KB->2KB) + hapus .ico palsu

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mindmate</title>
-    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
-<link rel="icon" type="image/png" href="{{ asset('favicon.png') }}" sizes="32x32">
+    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}" sizes="32x32">
+<link rel="shortcut icon" href="{{ asset('favicon.png') }}">
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
